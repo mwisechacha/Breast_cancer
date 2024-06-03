277c4a2fcb494ef35f96e90c59521e29d606277c
Angela Chacha <angieschacha@gmail.com>
2024-06-03 12:45:13 +0300

    deleted comments

--- static/index.js
+++ static/index.js
@@ -5,19 +5,19 @@ document.addEventListener('DOMContentLoaded', function () {
     var submitButton = document.getElementsByClassName('submit-button');
     var predictionForm = document.getElementById('predictionForm');
 
-    // Add event listener to the form submit event
+
     predictionForm.addEventListener('submit', function(event) {
-        event.preventDefault(); // Prevent the form from submitting and refreshing the page
+        event.preventDefault(); 
         
         display = true;
         console.log('Form was submitted!');
 
-        // Update resultElement when form is submitted
+
         resultElement.textContent = "Result";
         resultElement.classList.remove('hidden');
     });
 
-    // Check display when DOM is loaded (though it's redundant in this simplified case)
+
     if (display) {
         resultElement.textContent = "Result";
         resultElement.classList.remove('hidden');
